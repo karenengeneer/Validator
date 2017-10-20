3bba6ee260ac7e6eadf5480114ef764f536b661b
karenengeneer <31364906+karenengeneer@users.noreply.github.com>
2017-10-20 12:30:00 +0400

    Update email.js

--- email.js
+++ email.js
@@ -1,12 +1,12 @@
-const Validator =  require('./Validator.js');
-class URLValidator extends Validator{
-   isValURL(url){
-   if(!url)
-   return false;
-   let urlReg = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
-   if(urlReg.test(url))
-     return true;
-     return false;
- }
- }
- module.exports = email.js;
+ const Validator =  require('./Validator.js')
+ class EmailValidator extends Validator{
+    isValEmail(email){
+    if(!email)
+    throw new Error("Please enter your email address");
+    let addReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(isValEmail.test(email))
+    return true;
+    throw new Error("Incorrect email address");
+  }
+}
+module.exports = EmailValidator;
